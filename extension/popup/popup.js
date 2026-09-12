5d9e5e20fb31f79082f98ede2b370ab30b8048af
Keerthana <keerthana80306@gmail.com>
2026-09-12 15:41:17 +0530

    Update popup.js

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,8 +1,10 @@
 document.getElementById('start-btn').addEventListener('click', () => {
   const intent = document.getElementById('user-intent').value;
+  const accessibilityProfile = document.getElementById('access-profile').value;
   chrome.runtime.sendMessage({
     action: "START_COGNIA_SESSION",
-    goal: intent
+    goal: intent,
+    accessibility_profile: accessibilityProfile
   });
   window.close();
 });
